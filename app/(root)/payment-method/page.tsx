7cf9c42dcbdb6eb474d048e9e0e9724c666d17d0
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2025-10-03 01:05:50 +0100

    Payment Method schema and form
    - Schema and validators
    - Page and form component
    - User actions

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+export const metaData: Metadata = {
+  title: "Select Payment Method",
+};
+
+const PaymentMethodPage = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) throw new Error("User not found.");
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <h1>Payment Method</h1>
+      <CheckoutSteps current={2} />
+      <PaymentMethodForm preferredPaymentmethod={user.paymentMethod} />
+    </>
+  );
+};
+
+export default PaymentMethodPage;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema,
 } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -10,6 +11,7 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import z from "zod";
 
 //sign in user with credentials
 export async function signInWithCredentials(
@@ -122,6 +124,32 @@ export async function updateUserAddress(data: ShippingAddress) {
       data: { address },
     });
     return { success: true, message: "User address updated successfully." };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+//update users payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+    if (!currentUser) throw new Error("User not found");
+    const paymentMethod = paymentMethodSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+    return {
+      success: true,
+      message: "Payment method updated",
+    };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
