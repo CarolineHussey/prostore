42e95bcd66fc4150c005f57aeb273b5913606d65
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2025-10-13 14:08:25 +0100

    HomePage Carousel & Drawer menu
    Drawer
    - Added Category Drawer component and imported to header component
    
    Carousel
    - Installed embla-carousel-autoplay to handle carousel autoplay
    - Added carousel component & imported to homepage
    - Added getFeaturedProducts action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 const Home = async () => {
   const latestProducts = await getLatestProducts();
+  const featured = await getFeaturedProducts();
   return (
     <>
+      {featured.length > 0 && <ProductCarousel data={featured} />}
       <ProductList
         data={latestProducts}
         title="Featured Products"
