1e2b77d40e3c4d487b5dffc5647f4b3784c7b9a0
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2025-10-14 00:45:37 +0100

    Review set up
    - Prisma Model
    - Zod Validators
    - Review Type
    - Review list component started

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -6,6 +6,8 @@ import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
 import AddToCart from "@/components/shared/product/add-to-cart";
 import { getMyCart } from "@/lib/actions/cart.actions";
+import ReviewList from "./review-list";
+import { auth } from "@/auth";
 
 const ProductDetailsPage = async (props: {
   //type=promise
@@ -15,6 +17,9 @@ const ProductDetailsPage = async (props: {
   const product = await getSingleProductBySlug(slug);
 
   if (!product) notFound();
+
+  const session = await auth();
+  const userId = session?.user?.id;
 
   const cart = await getMyCart();
   const validCart =
@@ -88,6 +93,14 @@ const ProductDetailsPage = async (props: {
             </Card>
           </div>
         </div>
+      </section>
+      <section className="mt-10">
+        <h2 className="h2-bold">Customer Reviews</h2>
+        <ReviewList
+          productId={product.id}
+          userId={userId || ""}
+          productSlug={product.slug}
+        />
       </section>
     </>
   );
