bcda37f2718c08e0c5d4363efefd433d4af959a1
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2025-11-03 22:33:33 +0000

    Stripe Payment Setup

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -5,6 +5,7 @@ import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
 //import { ShippingAddress } from "@/types";
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -17,12 +18,29 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
 
   const session = await auth();
 
+  let client_secret = null;
+
+  //check if is not paid & is using stripe
+
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    //init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    //Create Payment Intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "GBP",
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
+
   return (
     <OrderDetailsTable
       order={{
         ...order,
         shippingAddress: order.shippingAddress as ShippingAddress,
       }}
+      stripeClientSecret={client_secret}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
       isAdmin={session?.user?.role === "admin" || false}
     />
